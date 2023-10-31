7c9dc8448ae8e4c9b9794fc69e71b2b503d7597b
RichardRyan141 <114993335+RichardRyan141@users.noreply.github.com>
2023-10-31 14:02:05 +0700

    Update Navbar & Sidebar

--- resources/views/admin/layouts/navbar.blade.php
+++ resources/views/admin/layouts/navbar.blade.php
@@ -64,7 +64,7 @@
                         class="absolute right-0 w-56 p-2 mt-2 space-y-2 text-gray-600 bg-white border border-gray-100 rounded-md shadow-md dark:text-gray-300 dark:border-gray-700 dark:bg-gray-700">
                         <li class="flex">
                             <a class="inline-flex items-center justify-between w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
-                                href="#">
+                                href="/admin/email">
                                 <span>Messages</span>
                                 <span
                                     class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-600 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-600">
@@ -84,7 +84,7 @@
                         </li>
                         <li class="flex">
                             <a class="inline-flex items-center justify-between w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
-                                href="#">
+                                href="/admin/alert">
                                 <span>Alerts</span>
                             </a>
                         </li>
@@ -108,7 +108,7 @@
                         aria-label="submenu">
                         <li class="flex">
                             <a class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
-                                href="#">
+                                href="/profile">
                                 <svg class="w-4 h-4 mr-3" aria-hidden="true" fill="none" stroke-linecap="round"
                                     stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                                     <path d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z">
--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -38,6 +38,36 @@
                 </a>
             </li>
             <li class="relative px-6 py-3">
+                @if (Request::is('admin/products*'))
+                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                        aria-hidden="true"></span>
+                @endif
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 {{ Request::is('admin/products*') ? 'text-gray-800 dark:text-gray-100' : '' }}"
+                    href="{{ route('admin.products.index') }}">
+                    <template x-if="!dark">
+                        <img src="{{ asset('assets/img/shopping-bag.png') }}" style="margin-left: 2px; width: 15px; height: 20px;" alt="Shopping Bag Icon">
+                    </template>
+                    <template x-if="dark">
+                        <img src="{{ asset('assets/img/invert-shopping-bag.png') }}" style="margin-left: 2px; width: 15px; height: 20px;" alt="Shopping Bag Icon">
+                    </template>
+                    <span class="ml-4">Product</span>
+                </a>
+            </li>
+            <li class="relative px-6 py-3">
+                @if (Request::is('admin/ecommerce*'))
+                    <span class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                        aria-hidden="true"></span>
+                @endif
+                <a class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 {{ Request::is('admin/ecommerce*') ? 'text-gray-800 dark:text-gray-100' : '' }}"
+                    href="{{ route('admin.ecommerce') }}">
+                    <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
+                        stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                        <path d="M4 6h16M4 10h16M4 14h16M4 18h16"></path>
+                    </svg>
+                    <span class="ml-4">E-Commerce</span>
+                </a>
+            </li>
+            <li class="relative px-6 py-3">
                 <button
                     class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                     @click="togglePagesMenu" aria-haspopup="true">
